cd452d38328f06a9e75abcfdc1e8d03b29a16b92
fcarlosdev <fcarlosbr@gmail.com>
2019-04-01 22:08:30 -0300

    Refactoring the code and improve the app

--- page_genarator/index.js
+++ page_genarator/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const path = require("path");
-const { parse } = require('querystring');
+const { parse } = require("querystring");
 
 const util = require("./libs/util");
 
@@ -9,54 +9,84 @@ const CHARSET = "utf8";
 const PORT = process.env.PORT || 5000;
 
 const createListMarkup = (page, link) => {
-
-  let OPEN_UL = util.leftSpace("<ul>",4);
-  let CLOSE_UL = util.leftSpace("</ul>",4)
+  
+  let OPEN_UL = util.leftSpace("<ul>", 4);
+  let CLOSE_UL = util.leftSpace("</ul>", 4);
 
   let elements = page.split("\n");
 
-  let newLink  = util.leftSpace(`<li>\n`,6);
-      newLink += util.leftSpace(`<a href ="${link.url}">${link.title}</a>\n`,8);
-      newLink += util.leftSpace(`</li>`,6);
+  let newLink = util.leftSpace(`<li>\n`, 6);
+  newLink += util.leftSpace(`<a href ="${link.url}">${link.title}</a>\n`, 8);
+  // newLink += util.leftSpace(`<button onclick="javascript:remove();"> Remove</button>\n`,8);
+  newLink += util.leftSpace(`</li>`, 6);
 
   if (elements.some(e => e.includes("<ul>"))) {
-    let liPos = elements.findIndex( e => e.includes("</ul>"));
-    elements.splice(liPos,0,newLink);
+    let liPos = elements.findIndex(e => e.includes("<ul>"));
+    elements.splice(liPos+1, 0, newLink);
   } else {
-      let from = (elements.findIndex(e => e.includes(`<div class="container">`))) + 1;
-      elements.splice(from++,0,OPEN_UL);
-      elements.splice(from++,0,newLink);
-      elements.splice(from++,0,CLOSE_UL);
+    let from =
+      elements.findIndex(e => e.includes(`<div class="container">`)) + 1;
+    elements.splice(from++, 0, OPEN_UL);
+    elements.splice(from++, 0, newLink);
+    elements.splice(from++, 0, CLOSE_UL);
   }
 
-  return elements.toString().replace(/>,/g,">\n");
-}
+  return elements.toString().replace(/>,/g, ">\n").replace(/,/g,"\n");
+};
 
+const getPostData = (req, callback) => {
+  
+  const FORM_URLENCODED = 'application/x-www-form-urlencoded';
 
-const server = http.createServer((req, res) => {
-  if (req.method === "POST") {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString(); // convert Buffer to string
+  if (req.headers['content-type'] === FORM_URLENCODED) {
+    let body = "";  
+    req.on("data", chunk => {    
+      body += chunk.toString();
     });
     req.on('end', () => {
-      let link_data = parse(body);
-      fs.readFile("./templates/template.html", (err,content) => {
-        if (err) throw err;
-        let data = createListMarkup(content.toString(),link_data);
-        fs.writeFile("./templates/template.html", data, function(err) {
-          if(err) console.log(err);
-        });
-        res.end(data, CHARSET);
-      })
+      callback(parse(body));
     });
   } else {
-    fs.readFile("./templates/template.html", (err,data) => {
-      if (err) throw err;
-      // data = changePageContent(data.toString());
-      // let htmlPage = data.toString().split("\n");
-      res.end(data, CHARSET);
+    callback(null);
+  }
+
+};
+
+const getFileContent = (file, callback) => {
+  let data;
+  fs.readFile(file, (err,content) => {
+    if (err) throw err;
+    callback(content);
+  });
+}
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    "templates",
+    req.url === "/" ? "template.html" : req.url
+  );
+
+  if (req.method === "POST") {
+    getPostData(req, postData => {      
+      getFileContent("./templates/template.html",result = (content) => {        
+        let data = createListMarkup(content.toString(),postData);        
+        fs.writeFile("./templates/template.html", data, function(err) {
+          if(err) throw err;
+        });
+        res.end(data, CHARSET);
+      });
     })
+  } else {
+    getFileContent(filePath, (content) => {
+      let ext = path.extname(filePath);
+      let contentType = "text/html";
+      if (ext === ".css") {
+        contentType = "text/css";
+      }
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, CHARSET);
+    });
   }
 });
 
