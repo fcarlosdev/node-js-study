157683535d7a72b337a09cc4a83f6c7345097460
fcarlosdev <fcarlosbr@gmail.com>
2019-04-05 17:27:35 -0300

    Improving the layout.

--- page_genarator/index.js
+++ page_genarator/index.js
@@ -7,6 +7,12 @@ const util = require("./libs/util");
 
 const CHARSET = "utf8";
 const PORT = process.env.PORT || 5000;
+const CARD_COLORS = ["blue-card", "red-card", "green-card", "orange-card"];
+
+const selectColor = () => {
+  let backColor = CARD_COLORS[Math.floor(Math.random() * Math.floor(6))];
+  return (backColor !== undefined) ? backColor : CARD_COLORS[0];
+}
 
 const createListMarkup = (page, link) => {
   
@@ -15,8 +21,13 @@ const createListMarkup = (page, link) => {
 
   let elements = page.split("\n");
 
-  let newLink = util.leftSpace(`<li>\n`, 6);
-  newLink += util.leftSpace(`<a href ="${link.url}">${link.title}</a>\n`, 8);
+  let newLink = util.leftSpace(`<li class="card ${selectColor()}">\n`, 6);
+  newLink += util.leftSpace(`<div class="card-head">\n`,8);
+  newLink += util.leftSpace(`<div class="btn-close">x</div>\n`,10);
+  newLink += util.leftSpace(`</div>\n`,8);
+  newLink += util.leftSpace(`<div class="link-title">\n`,8);
+  newLink += util.leftSpace(`<a href ="${link.url}">${link.title}</a>\n`, 10);
+  newLink += util.leftSpace(`</div>\n`,8);
   // newLink += util.leftSpace(`<button onclick="javascript:remove();"> Remove</button>\n`,8);
   newLink += util.leftSpace(`</li>`, 6);
 
@@ -69,7 +80,7 @@ const server = http.createServer((req, res) => {
 
   if (req.method === "POST") {
     getPostData(req, postData => {      
-      getFileContent("./templates/template.html",result = (content) => {        
+      getFileContent(filePath,result = (content) => {        
         let data = createListMarkup(content.toString(),postData);        
         fs.writeFile("./templates/template.html", data, function(err) {
           if(err) throw err;
